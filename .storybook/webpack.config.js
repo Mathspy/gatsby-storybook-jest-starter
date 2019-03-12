3e705e01554debd34142d6e038109f8341ab2c67
Elie <eliesteinbock@gmail.com>
2019-03-12 17:35:54 +0200

    updated to work with storybook v5 (#10)

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,19 +1,23 @@
-module.exports = (baseConfig, env, defaultConfig) => {
+module.exports = ({ config, mode }) => {
   // Transpile Gatsby module because Gastby includes un-transpiled ES6 code.
-  defaultConfig.module.rules[0].exclude = [/node_modules\/(?!(gatsby)\/)/]
+  config.module.rules[0].exclude = [/node_modules\/(?!(gatsby)\/)/]
   // use installed babel-loader which is v8.0-beta (which is meant to work with @babel/core@7)
-  defaultConfig.module.rules[0].use[0].loader = require.resolve('babel-loader')
+  config.module.rules[0].use[0].loader = require.resolve('babel-loader')
 
   // The next two lines should always reflect the config in jest-preprocess.js until there is a way for Gatsby to expose an internal webpack.config
   // use @babel/preset-react for JSX and env (instead of staged presets)
-  defaultConfig.module.rules[0].use[0].options.presets = [
+  config.module.rules[0].use[0].options.presets = [
     require.resolve('@babel/preset-react'),
     require.resolve('@babel/preset-env'),
   ]
   // use @babel/plugin-proposal-class-properties for class arrow functions
-  defaultConfig.module.rules[0].use[0].options.plugins = [
+  config.module.rules[0].use[0].options.plugins = [
     require.resolve('@babel/plugin-proposal-class-properties'),
   ]
 
-  return defaultConfig
+  // https://github.com/gatsbyjs/gatsby/issues/10662:
+  // Prefer Gatsby ES6 entrypoint (module) over commonjs (main) entrypoint
+  config.resolve.mainFields = ["browser", "module", "main"]
+
+  return config
 }
